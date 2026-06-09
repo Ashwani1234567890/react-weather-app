605121fd2e1287591dd5a4817a13451d381909b1
Ashwani1234567890 <yashwanitiwari23@gmail.com>
2026-06-09 17:01:15 +0530

    Initial Commit

---
+++ Weather/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import Weather0 from './components/weather'
+
+function App() {
+  return (
+    <div className='app'>
+  <Weather0/>
+  </div>
+)
+}
+
+export default App
